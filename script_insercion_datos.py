76479d8be202fca6ce9fa13748a7f4913c68e1ac
Juan Jose Lopez Roldan <jjlr1994@gmail.com>
2017-11-13 21:13:13 +0100

    modf. fichero

--- script_insercion_datos.py
+++ script_insercion_datos.py
@@ -1,9 +1,13 @@
 #-*-coding: utf-8 -*-
 import sys,os
 opcion1=sys.argv[1]
-opcion2=sys.argv[2]
-opcion3=sys.argv[3]
-opcion4=sys.argv[4]
+tipo=sys.argv[2]
+nombre=sys.argv[3]
+dato=sys.argv[4]
 
 if opcion1 == '-a':
-		directa = open('/var/cache/bind/db.lopez.gonzalonazareno.org')
+	listad = open('/var/cache/bind/db.lopez.gonzalonazareno.org', 'a')
+	listai = open('/var/cache/bind/db.172.22.200', 'a')
+
+	if tipo == 'dir':
+
