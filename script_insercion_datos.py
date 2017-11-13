7e518cf7984f9977d71d0f76123dfbcf4c5e09e1
Juan Jose Lopez Roldan <jjlr1994@gmail.com>
2017-11-13 22:20:22 +0100

    modf. final fichero

--- script_insercion_datos.py
+++ script_insercion_datos.py
@@ -2,18 +2,17 @@
 import sys,os
 opcion1=sys.argv[1]
 tipo=sys.argv[2]
-nombre=sys.argv[3]
-dato=sys.argv[4]
 
 if opcion1 == '-a':
 	listad = open('/var/cache/bind/db.lopez.gonzalonazareno.org', 'a')
 	listai = open('/var/cache/bind/db.172.22.200', 'a')
-
+	nombre=sys.argv[3]
+	dato=sys.argv[4]
 	if tipo == '-dir':
 		listad.write(nombre+'	IN	A	'+dato+'\n')
 		listai.write(dato+'	IN	PTR	'+nombre+'.lopez.gonzalonazareno.org.\n')
 	elif tipo == '-alias':
-		listad.write(nombre+'	IN	CNAME	'+tipo+'.lopez.gonzalonazareno.org.\n')
+		listad.write(nombre+'	IN	CNAME	'+dato+'.lopez.gonzalonazareno.org.\n')
 	listad.close()
 	listai.close()
 elif opcion1 == '-b':
