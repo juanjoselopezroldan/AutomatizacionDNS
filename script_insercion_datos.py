6156e48bd9299d43daf341bfa9f36e3391bc8cc8
Juan Jose Lopez Roldan <jjlr1994@gmail.com>
2017-11-13 17:56:13 +0100

    insercion de variables

--- script_insercion_datos.py
+++ script_insercion_datos.py
@@ -1,4 +1,15 @@
 #-*-coding: utf-8 -*-
 import sys,commands
 
-print "Bienvenido al script que te permite realizar la insercion de datos "
+print "Bienvenido al script que te permite realizar la insercion de datos"
+
+opcion1=str(raw_input("Introduce opcion -a (Añadirá nuevo registro) o -b (Eliminar registro): "))
+if opcion1 == "-a":
+	opcion2=str(raw_input("Introduce opcion -dir (Creará registro A) o -alias (Creará registro CNAME): "))
+
+opcion3=str(raw_input("Introduce nombre maquina para añadir o borrar: "))
+
+if opcion2 == "-dir":
+	opcion4=str(raw_input("Introduce D. Ip: "))
+else:
+	opcion5=str(raw_input("Introduce N. Alias: "))
