9b62e63ce43e24fa50d70ad3e9d22125388e0cf1
Juan Jose Lopez Roldan <jjlr1994@gmail.com>
2017-11-13 21:47:02 +0100

    modf. final fichero

--- script_insercion_datos.py
+++ script_insercion_datos.py
@@ -9,8 +9,14 @@ if opcion1 == '-a':
 	listad = open('/var/cache/bind/db.lopez.gonzalonazareno.org', 'a')
 	listai = open('/var/cache/bind/db.172.22.200', 'a')
 
-	if tipo == 'dir':
+	if tipo == '-dir':
 		listad.write(nombre+'	IN	A	'+dato+'\n')
 		listai.write(dato+'	IN	PTR	'+nombre+'.lopez.gonzalonazareno.org.\n')
-	
-
+	elif tipo == '-alias':
+		listad.write(nombre+'	IN	CNAME	'+tipo+'.lopez.gonzalonazareno.org.\n')
+	listad.close()
+	listai.close()
+elif opcion1 == '-b':
+	os.system("sed -i '/^"+tipo+".*$/d' /var/cache/bind/db.lopez.gonzalonazareno.org")
+	os.system("sed -i '/"+tipo+".*$/d' /var/cache/bind/db.172.22.200")
+os.system('systemctl restart bind9')
